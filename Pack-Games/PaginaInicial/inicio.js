5567dfcdb6de64286e1cff58a37c1efa02477c44
Rafaelgu27 <127206586+Rafaelgu27@users.noreply.github.com>
2024-09-18 11:54:31 -0300

    Add files via upload

--- Pack-Games/PaginaInicial/inicio.js
+++ Pack-Games/PaginaInicial/inicio.js
@@ -7,20 +7,17 @@ document.addEventListener('DOMContentLoaded', function() {
         pacmanLink.addEventListener('click', function(event) {
             event.preventDefault();
 
-            // Torna a animação principal visível e inicia a transição
             pacmanAnimacao.classList.add('show');
-            pacmanAnimacao.style.left = '200vw'; // Move o Pac-Man para fora da tela
+            pacmanAnimacao.style.left = '200vw';
 
-            // Inicia a animação dos fantasmas após um pequeno delay
             setTimeout(() => {
-                pacmanAnimacao2.style.right = '200vw'; // Move os fantasmas da direita para a esquerda
+                pacmanAnimacao2.style.right = '200vw';
                 pacmanAnimacao2.classList.add('show');
-            }, 5000); // Pequeno delay para garantir que a animação dos fantasmas comece após o Pac-Man
+            }, 5000); 
 
-            // Redireciona após a animação
             setTimeout(() => {
                 window.location.href = pacmanLink.href;
-            }, 12000); // Tempo deve ser igual ao da transição de `left` e `right`
+            }, 12000);
         });
     } else {
         console.error('Elementos do Pac-Man não encontrados.');
